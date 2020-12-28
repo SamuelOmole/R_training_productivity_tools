89a7bea057489aeb8a7c1c6270fbb0ee130ea5c1
Your Name <your@email.com>
2020-12-28 17:05:50 +0000

    corrected the typo and loaded tidyverse library

--- Git.R
+++ Git.R
@@ -1,6 +1,6 @@
 # Git ---------------------------------------------------------------------
-# the following lines of codes tell Guthub who we are
+# the following lines of codes tell Github who we are
 # git config --global user.name "Samuel Omole"
 # git config --global user.email "so444@bath.ac.uk"
 
-
+library(tidyverse)
