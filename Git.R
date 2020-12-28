402051b0756dded751024c61adf6e26fce1325cc
Samuel Omole <so444@bath.ac.uk>
2020-12-28 02:24:22 +0000

    Committing my first file with Git

---
+++ Git.R
@@ -0,0 +1,6 @@
+# Git ---------------------------------------------------------------------
+# the following lines of codes tell Guthub who we are
+# git config --global user.name "Samuel Omole"
+# git config --global user.email "so444@bath.ac.uk"
+
+
