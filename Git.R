9e80b38411c9fb869095e2f98366966cb4a3d3da
Your Name <your@email.com>
2020-12-29 01:05:55 +0000

    Final commit for this note

--- Git.R
+++ Git.R
@@ -29,3 +29,25 @@ git add README.txt # staging
 git commit -m "First commit. Adding README.txt file just to get started"
 git remote add origin "githuburl" # to link the local and upstream repo
 git push    # you may need to add these arguments the first time: --set-upstream origin master
+
+
+# To get help on Unix, especially in compact implementation of Unix such as Gitbash, the command-name--help works
+# e.g. ls --help provides help on the ls command. to get more concise help we use ls--help|less
+
+# wildcards are powerful aspects of Unix. Examples include * and ?
+# for example ls *.html lists all html files in a directory while rm *.html removes all html files
+# if we know that files have the format, say, file-001.html and we know there are 3 characters, 
+# then we use rm file-???.html to remove all files with similar format
+# wildcards can be combined as in rm file-???.* which removes all files of that format regardless of suffix
+
+
+# Some advanced Unix commands which can come in handy include: 
+# start filename -- this opens the file with a suitable application
+# nano filename -- command line text editor
+# ln -- creates symbolic links
+# tar -- archive files and subdirectories of a directory into one file
+# ssh -- connect to another computer
+# grep - search for patterns in a file
+# awk/sed - These are two very powerful commands that permit you to find specific strings in files and change them
+
+
